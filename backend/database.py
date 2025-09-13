350367ef5414f7f763d3b1d435143db84f444ad8
Guilherme <engguilhermen@gmail.com>
2025-09-12 23:19:40 -0300

    Create database.py

---
+++ backend/database.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+
+# Database URL from environment variable Docker
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@db:5432/dbname")
+
+# Create the database engine
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# Create a configured "Session" class
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Create a base class for declarative class definitions
+Base = declarative_base()

8b4f4a8fe543a32d8c6b0afb1e331e93243cfbc4
Guilherme <engguilhermen@gmail.com>
2025-09-12 23:21:52 -0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,10 @@
 from fastapi import FastAPI
+from . import models  # Importa os modelos
+from .database import engine # Importa a engine
+
+# Este comando instrui o SQLAlchemy a criar todas as tabelas no banco
+# com base nos modelos que definimos
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="Meu Corre | Vendas na Mão",
@@ -7,4 +13,6 @@ app = FastAPI(
 
 @app.get("/")
 def read_root():
-    return {"status": "API 'Meu Corre' começou!", "ambiente": "Docker"}
+    return {"status": "API 'Meu Corre' no ar!", "ambiente": "Docker"}
+
+# Aqui vamos adicionar o endpoint para criar clientes no próximo passo!

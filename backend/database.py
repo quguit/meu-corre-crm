194084a8a0b36f61844dc33c57c6e06ce92d46f4
Guilherme <engguilhermen@gmail.com>
2025-09-20 22:56:23 -0300

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -3,8 +3,17 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+# Database URL from environment variable Docker
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://user:password@db:5432/dbname"
+)
 
-engine = create_engine(DATABASE_URL)
+# Create the database engine
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# Create a configured "Session" class
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Create a base class for declarative class definitions
 Base = declarative_base()
